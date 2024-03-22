5e65e7cbb66d6b3f84fbd3bdb4b2cfd376381fc0
nrmn3m <nabdmonem@kwentra.com>
2024-03-22 21:21:14 +0200

    Solving 121-best-time-to-buy-sell-stock using Golang

---
+++ 121-best-time-to-buy-sell-stock/121-best-time-to-buy-sell-stock_test.go
@@ -0,0 +1,34 @@
+package leetcode
+
+import "testing"
+
+func Test_maxProfit(t *testing.T) {
+	type args struct {
+		prices []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Example 1",
+			args: args{prices: []int{7, 1, 5, 3, 6, 4}},
+			want: 5,
+		},
+		{
+			name: "Example 2",
+			args: args{prices: []int{7, 6, 4, 3, 1}},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			if got := maxProfit(tt.args.prices); got != tt.want {
+				t.Errorf("maxProfit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
