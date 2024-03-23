577919c7108440df4cabd43090535aced755ea26
nrmn3m <nabdmonem@kwentra.com>
2024-03-23 21:02:36 +0200

    Solving 234-palindrome-linkedlist using Golang

---
+++ 234-palindrome-linkedlist/234-palindrome-linkedlist.go
@@ -0,0 +1,39 @@
+package leetcode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func isPalindrome(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return true
+	}
+
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	var prev *ListNode
+	current := slow
+	for current != nil {
+		next := current.Next
+		current.Next = prev
+		prev = current
+		current = next
+	}
+	secondHalf := prev
+
+	firstHalf := head
+	for secondHalf != nil {
+		if firstHalf.Val != secondHalf.Val {
+			return false
+		}
+		firstHalf = firstHalf.Next
+		secondHalf = secondHalf.Next
+	}
+
+	return true
+}
---
+++ 234-palindrome-linkedlist/234-palindrome-linkedlist_test.go
@@ -0,0 +1,56 @@
+package leetcode
+
+import "testing"
+
+func Test_isPalindrome(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Palindrome",
+			args: args{
+				head: &ListNode{
+					Val: 1,
+					Next: &ListNode{
+						Val: 2,
+						Next: &ListNode{
+							Val: 2,
+							Next: &ListNode{
+								Val:  1,
+								Next: nil,
+							},
+						},
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "Non-palindrome",
+			args: args{
+				head: &ListNode{
+					Val: 1,
+					Next: &ListNode{
+						Val:  2,
+						Next: nil,
+					},
+				},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			if got := isPalindrome(tt.args.head); got != tt.want {
+				t.Errorf("isPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
