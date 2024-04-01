44c52fccd70da0448ee9bbd657e4a29daa99eb32
nrmn3m <nabdmonem@kwentra.com>
2024-04-01 21:14:17 +0200

    Solving 151-reverse-words-in-string using Golang

---
+++ leetcode75/array-string/151-reverse-words-in-string/151-reverse-words-in-string_test.go
@@ -0,0 +1,34 @@
+package leetcode75
+
+import "testing"
+
+func Test_reverseWords(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "Example one",
+			args: args{s: "leet code"},
+			want: "code leet",
+		},
+		{
+			name: "Example two",
+			args: args{s: "mn3m nour"},
+			want: "nour mn3m",
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			if got := reverseWords(tt.args.s); got != tt.want {
+				t.Errorf("reverseWords() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
